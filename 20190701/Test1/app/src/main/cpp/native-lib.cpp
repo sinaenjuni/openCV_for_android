fc3cc9703164e61957c7bbb9b1ebdb5adf722373
y2k4388 <y2k4388@gmail.com>
2019-07-03 00:56:11 +0900

    'second'

--- 20190701/Test1/app/src/main/cpp/native-lib.cpp
+++ 20190701/Test1/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,16 @@
 #include <jni.h>
-#include <string>
+#include <opencv2/opencv.hpp>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_textcode001_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+using namespace cv;
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_textcode001_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject instance, jlong matAddrInput, jlong matAddrResult) {
+
+    Mat &matInput = *(Mat *)matAddrInput;
+    Mat &matResult = *(Mat *)matAddrResult;
+
+    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
+
 }
